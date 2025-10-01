87eacafb45686baab65548263b3566d8ebbf6afd
Oscar Domínguez Alonso <iamoscaar@gmail.com>
2025-10-01 20:09:43 +0200

    msg

--- Proyectos Clase/pruebagithub/src/App.java
+++ Proyectos Clase/pruebagithub/src/App.java
@@ -7,6 +7,8 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
 
-       
+       System.out.println("Hola, soy Oscar y estoy modificando el proyecto");
+       System.out.println("Hola, soy Oscar y estoy modificando el proyecto");
+
     }
 }
