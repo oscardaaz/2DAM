df087727cb4f821c94d9c4ef88e391ff20b53136
Oscar Domínguez Alonso <iamoscaar@gmail.com>
2025-10-16 20:51:40 +0200

    Fixes in code

--- Programacion_procesos_servicios/programa1/src/programa1/p1.java
+++ Programacion_procesos_servicios/programa1/src/programa1/p1.java
@@ -2,46 +2,35 @@ package programa1;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 public class p1 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         ProcessBuilder pb = new ProcessBuilder("CMD", "/C", "DIR");
-        Process p = pb.start();
+        try {
+            Process p = pb.start();
 
-
-        try (InputStream is = p.getInputStream()) {
-
-            int c;
-            while ((c = is.read()) != -1)
-                System.out.print((char) c);
-
-        } catch (IOException e) {
-            System.err.println("Error " + e.getMessage());
-        }
-
-
-        try (OutputStream os = p.getOutputStream()) {
-            os.write("16/10/2025".getBytes());
-            os.flush();
-        } catch (IOException e) {
-            System.err.println("Error " + e.getMessage());
-        }
-
-
-        try (InputStream er = p.getInputStream()) {
-            int c;
-            while ((c = er.read()) != -1) {
-                System.out.print((char) c);
+            // Leer salida estándar
+            try (InputStream is = p.getInputStream()) {
+                int c;
+                while ((c = is.read()) != -1) {
+                    System.out.print((char) c);
+                }
             }
-            int exitVal = p.waitFor();
-            System.out.println("Valores de salida: " + exitVal);
-        } catch (IOException e) {
-            System.err.println("Error " + e.getMessage());
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
 
+            // Leer salida de error
+            try (InputStream es = p.getErrorStream()) {
+                int c;
+                while ((c = es.read()) != -1) {
+                    System.err.print((char) c);
+                }
+            }
+
+            int exitVal = p.waitFor();
+            System.out.println("\nValor de salida: " + exitVal);
+
+        } catch (IOException | InterruptedException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
