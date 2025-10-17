9db57f0fbfaa9d7a54586a77cce85c400b8d0c27
Oscar Domínguez Alonso <iamoscaar@gmail.com>
2025-10-17 17:39:30 +0200

    Actualizacion ejercicio AD

--- Ejercicios_AD/ActividadesFichBinarios/src/datosBinarios/gestionEmpleados.java
+++ Ejercicios_AD/ActividadesFichBinarios/src/datosBinarios/gestionEmpleados.java
@@ -5,6 +5,7 @@ import Objetos.Empleado;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -20,35 +21,54 @@ public class gestionEmpleados {
         //leerEmpleados();
     }
 
-
+    private static int cantidadEmpleados(){
+        System.out.println("Introduce la cantidad de empleados a añadir");
+        int cantidadEmpleados = sc.nextInt();
+        sc.nextLine();
+        return cantidadEmpleados;
+    }
 
     private static void escribirEmpleado() {
 
         Path ruta = obtenerRutaFichero("Escribe el nombre del fichero a escribir: ");
+        boolean existe = Files.exists(ruta);
 
-        try (OutputStream os = Files.newOutputStream(ruta);
-             ObjectOutputStream oos = new ObjectOutputStream(os)) {
+        try (OutputStream os = Files.newOutputStream(
+                ruta,
+                StandardOpenOption.APPEND,
+                StandardOpenOption.CREATE);
+             ObjectOutputStream oos = existe //Valor booleano true/false para si existe se hace sin cabecera y no da error..
+                     ? new ObjectOutputStreamSinCabecera(os)
+                     : new ObjectOutputStream(os)) {
 
-            for (int i = 1; i <= 3; i++) {
+            int cantidadEmpleados = existe ? cantidadEmpleados() : 3;
+            String mensaje = existe ? " a añadir: "
+                                    : ": " ;
+            String mensajeFichero = existe  ? "Fichero sobreescrito correctamente"
+                                            : "Fichero escrito correctamente" ;
 
-                System.out.print("Introduce el nombre del empleado: ");
+            for (int i = 1; i <= cantidadEmpleados; i++) {
+
+                System.out.print("Introduce el nombre del empleado" + mensaje);
                 String nombre = sc.nextLine();
 
-                System.out.print("Introduce el departamento del empleado: ");
+                System.out.print("Introduce el departamento del empleado" + mensaje);
                 int departamento = sc.nextInt();
 
-                System.out.print("Introduce el salario del empleado: (Separador de decimales con punto ...");
+                System.out.print("Introduce el salario del empleado"+mensaje);
                 double salario = sc.nextDouble();
                 sc.nextLine();
 
                 Empleado empleado = new Empleado(nombre, departamento, salario);
                 oos.writeObject(empleado);
-                System.out.printf("Persona %d escrita correctamente%n", i);
+                System.out.printf("Empleado %d escrito correctamente%n", i);
 
             }
 
+            System.out.println(mensajeFichero);
+
         } catch (IOException ioe) {
-            System.err.println("Error al escribir Persona " + ioe.getMessage());
+            System.err.println("Error al escribir el Empleado " + ioe.getMessage());
         }
     }
 
@@ -78,7 +98,7 @@ public class gestionEmpleados {
 
 
         } catch (IOException e) {
-            System.err.println("Error al leer Persona " + e.getMessage());
+            System.err.println("Error al leer el Empleado " + e.getMessage());
         }
     }
 
@@ -89,4 +109,15 @@ public class gestionEmpleados {
         return Path.of(System.getProperty("user.dir"),nombreFichero);
     }
 
+    private static class ObjectOutputStreamSinCabecera extends ObjectOutputStream{
+
+        public ObjectOutputStreamSinCabecera(OutputStream out) throws IOException {
+            super(out);
+        }
+
+        @Override
+        protected void writeStreamHeader() {
+            //No hacemos nada
+        }
+    }
 }
