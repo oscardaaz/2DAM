c3f1a86695dd14d13c43c600296de16fc5ab44aa
Oscar Domínguez Alonso <iamoscaar@gmail.com>
2025-10-17 21:04:56 +0200

    Ejercicios kotlin

---
+++ KotlinPMDM/ejercicios/src/ejercicios/ejercicios.kt
@@ -0,0 +1,56 @@
+package ejercicios
+
+
+fun main() {
+
+    //ejercicio1()
+    //ejercicio2()
+    ejercicio3()
+    //ejercicio4()
+    //ejercicio5()
+    //ejercicio6()
+    //ejercicio7()
+    //ejercicio8()
+
+}
+
+fun ejercicio1() {
+
+    val a = 6
+    val b = 2
+    println("Suma:  ${a + b}")
+    println("Resta:  ${a - b}")
+    println("Multiplicacion:  ${a * b}")
+    println("Division:  ${a / b}")
+    println("Modulo:  ${a % b}")
+
+
+}
+
+fun ejercicio2() {
+
+    val nombre = "Oscar"
+    println("Bienvenido, $nombre")
+}
+
+fun ejercicio3() {
+
+    print("Introduce tu nombre: ")
+    val nombre = readln() + "\n"
+    println("Bienvenido, $nombre")
+
+}
+
+fun ejercicio4() {
+
+
+}
+
+fun ejercicio5() {
+
+}
+
+fun ejercicio6() {
+
+}
+
