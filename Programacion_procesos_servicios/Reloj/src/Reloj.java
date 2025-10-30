31a960026a9c1514790683b76b47fd177f9c8c9f
Oscar Domínguez Alonso <iamoscaar@gmail.com>
2025-10-30 20:59:16 +0100

    psp

--- Programacion_procesos_servicios/Reloj/src/Reloj.java
+++ Programacion_procesos_servicios/Reloj/src/Reloj.java
@@ -1,8 +1,28 @@
 import javax.swing.*;
+import java.util.Calendar;
 
 public class Reloj extends Thread {
 
     private JLabel label;
 
+    public Reloj (JLabel jlabel){
 
+        this.label = label;
+    }
+
+    public void run(){
+        while (true){
+            try{
+                Calendar c = Calendar.getInstance();
+                int hora = c.get(Calendar.HOUR);
+                int minutos = c.get(Calendar.MINUTE);
+                int segundos = c.get(Calendar.SECOND);
+                String tiempo = String.format("%d:%d:%d",hora,minutos,segundos);
+                this.label.setText(tiempo);
+                Thread.sleep(1000);
+            }catch(InterruptedException ex){
+                System.err.println("Error del hilo reloj");
+            }
+        }
+    }
 }
