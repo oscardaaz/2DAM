2fe2a331d0dee6778e3be48c9096cda42ab81fb8
Oscar Domínguez Alonso <iamoscaar@gmail.com>
2025-11-04 17:26:33 +0100

    Act Ej AD AccesoAleatorio 11

--- Ejercicios_AD/ActividadesFichAleatorios/src/Actividad11/actividad11.java
+++ Ejercicios_AD/ActividadesFichAleatorios/src/Actividad11/actividad11.java
@@ -1,6 +1,7 @@
 package Actividad11;
 
 import java.nio.file.Path;
+import java.util.Scanner;
 
 public class actividad11 {
     /**
@@ -21,17 +22,37 @@ public class actividad11 {
      */
 
     private static final Path RUTA = Path.of("src/Actividad11", "ficheroActividad11.dat");
+    private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println();
+        menu();
 
     }
 
-    private void menu(){
+    private static void menu(){
+        int opcion;
         do {
+            String cadena = """
+                    1. Añadir número de tipo float al final del fichero.
+                    
+                    2. Buscar en el fichero un número que se pedirá por consola. Si se encuentra, se
+                    sustituirá por otro número que también se pedirá por consola. Si no, se mostrará un
+                    mensaje indicando que el valor no se encuentra en el fichero
+                    
+                    3. Sustituir el número de la posición indicada (pedida por consola), si existe la posición,
+                    por otro número que también se pida por consola.
+                  
+                    4. Mostrar el fichero creado.
+                    
+                    5. Salir
+                    """;
+            System.out.println(cadena);
+            System.out.print("Introduce una opción: ");
+            opcion = sc.nextInt();
+        }while (opcion != 5);
+        System.out.println("Saliendo...");
 
-
-        }while (opcion != 5)
     }
 
 
