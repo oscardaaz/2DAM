5846cbfa8601c4af90092aa74c240899c8d70edf
Oscar Domínguez Alonso <iamoscaar@gmail.com>
2026-01-28 15:58:17 +0100

    fix

--- Programacion_procesos_servicios/UD4/Unidad4Ejemplos/src/Ejemplos/ClienteFTP2irDirectorio.java
+++ Programacion_procesos_servicios/UD4/Unidad4Ejemplos/src/Ejemplos/ClienteFTP2irDirectorio.java
@@ -2,61 +2,70 @@ package Ejemplos;
 
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
-
 import java.io.IOException;
 
-
 public class ClienteFTP2irDirectorio {
-  public static void main(String[] args) {
-	FTPClient cliente = new FTPClient();
-	String servFTP = "ftp.rediris.es";
-	System.out.println("Nos conectamos a: " + servFTP);
-	String usuario = "anonymous";
-	String clave = "anonymous";
-	try {
-		cliente.connect(servFTP);
-        cliente.enterLocalPassiveMode(); //modo pasivo
+    public static void main(String[] args) {
+        FTPClient cliente = new FTPClient();
 
-		boolean login = cliente.login(usuario, clave);
-		if (login)
-			System.out.println("Login correcto...");
-		else {
-			System.out.println("Login Incorrecto...");
-			cliente.disconnect();
-			System.exit(1);
-		}
-		System.out.println("Directorio actual: "
-				         + cliente.printWorkingDirectory());
-		
-		//ir a otro directorio
-		String directorio="/mirror/MySQL/Downloads/";
-		if(cliente.changeWorkingDirectory (directorio))
-		  System.out.println("Dir Actual:"+ cliente.printWorkingDirectory());
-		else
-		  System.out.println("NO EXISTE EL DIRECTORIO: "+directorio);
+        try {
+            // ========== ftp.ujaen.es ==========
+            System.out.println("\n=== Conectando a ftp.ujaen.es ===");
+            cliente.connect("ftp.ujaen.es");
+            cliente.enterLocalPassiveMode();
+            cliente.login("anonymous", "anonymous");
 
-			
-		FTPFile[] files = cliente.listFiles();
-		System.out.println("Ficheros en el directorio actual:"
-					+ files.length);
-            //array para visualizar el tipo de fichero
-		String tipos[] = {"Fichero", "Directorio","Enlace simb."};
-		
-		for (int i = 0; i < files.length; i++) {
-			System.out.println("\t" + files[i].getName() + " => "
-					+ tipos[files[i].getType()]);
-		}
-		boolean logout = cliente.logout();
-		if (logout) 
-			System.out.println("Logout del servidor FTP...");
-                else 
-		        System.out.println("Error al hacer Logout...");
-		//
-		cliente.disconnect();
-		System.out.println("Desconectado...");
-	} catch (IOException ioe) {
-		ioe.printStackTrace();
-	}		
-  }
-}// ..
+            // Listar /pub/linux/ofimatica/
+            cliente.changeWorkingDirectory("/pub/linux/ofimatica/");
+            listarDirectorio(cliente);
 
+            // Listar /pub/windows/
+            cliente.changeWorkingDirectory("/pub/windows/");
+            listarDirectorio(cliente);
+
+            // Listar /pub/windows/electronica/
+            cliente.changeWorkingDirectory("/pub/windows/electronica/");
+            listarDirectorio(cliente);
+
+            cliente.disconnect();
+
+            // ========== ftp.nluug.nl ==========
+            System.out.println("\n=== Conectando a ftp.nluug.nl ===");
+            cliente.connect("ftp.nluug.nl");
+            cliente.enterLocalPassiveMode();
+            cliente.login("anonymous", "anonymous");
+
+            cliente.changeWorkingDirectory("/pub/");
+            listarDirectorio(cliente);
+
+            cliente.disconnect();
+
+            // ========== test.rebex.net ==========
+            System.out.println("\n=== Conectando a test.rebex.net ===");
+            cliente.connect("test.rebex.net");
+            cliente.enterLocalPassiveMode();
+            cliente.login("demo", "password"); // Credenciales específicas
+
+            cliente.changeWorkingDirectory("/pub/example/");
+            listarDirectorio(cliente);
+
+            cliente.disconnect();
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    // Método auxiliar para listar directorios
+    private static void listarDirectorio(FTPClient cliente) throws IOException {
+        System.out.println("Directorio: " + cliente.printWorkingDirectory());
+        FTPFile[] files = cliente.listFiles();
+        System.out.println("Ficheros: " + files.length);
+
+        String[] tipos = {"Fichero", "Directorio", "Enlace simb."};
+        for (FTPFile file : files) {
+            System.out.println("  " + file.getName() + " - " + tipos[file.getType()]);
+        }
+        System.out.println();
+    }
+}
